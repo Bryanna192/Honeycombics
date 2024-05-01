764ccfe49589a56754990a750a9054794a78187a
Bryanna192 <bryannasanchez12@gmail.com>
2024-04-30 23:14:20 -0400

    Adjusted the registration container

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
-const iconClose = document.querySelector('.icon-close');
+const iconClose = document.querySelector('.icon-close ion-icon');
 
 registerLink.addEventListener('click', ()=> {
     wrapper.classList.add('active');
@@ -16,6 +16,6 @@ btnPopup.addEventListener('click', ()=> {
     wrapper.classList.add('active-popup');
 });
 
-iconClose.addEventListener('click', ()=> {
+iconClose.addEventListener('mousedown', ()=> {
     wrapper.classList.remove('active-popup');
 });
